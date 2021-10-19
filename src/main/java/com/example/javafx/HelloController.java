9c77f28629bfffcd3f4d4ce30ba2dd07f755d498
cedricklubin <clubin39@gmail.com>
2021-10-18 23:58:17 -0400

    Initial commit

---
+++ src/main/java/com/example/javafx/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        var newFont= Font.font(24);
+        welcomeText.setFont(newFont);
+        var textColor= Paint.valueOf("Green");
+        welcomeText.setTextFill(textColor);
+        welcomeText.setText("Welcome to JavaFX Comp 152!!");
+    }
+}
---
+++ src/main/java/com/example/javafx/SecondWindow.java
@@ -0,0 +1,14 @@
+package com.example.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SecondWindow {
+    @FXML
+    private Label ourMessage;
+
+    @FXML
+    public void handleButtonClick(){
+
+    }
+}
